315dae9f86969b0a443f2bc40d6f850ecb5402bd
Brian <thinhle1199@gmail.com>
2024-07-30 15:56:24 -0500

    rebase

---
+++ src/models/Car.ts
@@ -0,0 +1,12 @@
+export interface Car {
+    id: number;
+    model: string;
+    brand: string;
+    type: string;
+    transmission: string;
+    capacity: string;
+    bags: string;
+    pricePerDay: number;
+    totalPrice: number;
+    image: string;
+  }
